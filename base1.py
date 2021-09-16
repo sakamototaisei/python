8ba368dde253d6ac348f92d25d742cdbb1242267
sakamototaisei <taisei0403089@gmail.com>
2021-09-16 19:01:36 +0900

    study 55

--- base1.py
+++ base1.py
@@ -213,3 +213,191 @@
 
 # print('value = {}, type = {}'.format(int_msg, type(int_msg)))
 # print('value = {}, type = {}'.format(float_msg, type(float_msg)))
+# --------------------------------------------------------------------
+
+# リスト
+# list_a = [1,2,3,4]
+# list_b = []
+
+# print(list_a)
+# print(list_a[-1])
+
+# list_a = [1, [1,2,'apple'],3,'banana']
+# print(list_a[1][2])
+# print(list_a)
+# list_a[1][2] = 'lemon'
+# print(list_a)
+
+# リストのメソッド
+# list_a = [1,2,3,4,5]
+
+# list_b = list_a[1:4]
+# print(list_b)
+
+# # append(要素の追加), extend(リストの拡張), insert(特定の場所に要素を追加), clear(リストのクリア)
+# list_a.append('apple')
+# print(list_a)
+# list_a.extend(['banana', 'melon'])
+# print(list_a)
+# list_a.insert(1, 'grape')
+# print(list_a)
+# list_a.clear()
+# print(list_a)
+
+# remove(要素の削除), pop(要素の取り出し), count(要素のカウント), index(要素の検索)
+# list_a = [0,1,1,2,2,3,3,3,4]
+# list_a.remove(3)
+# print(list_a)
+
+# value = list_a.pop()
+# print(list_a, value)
+
+# print(list_a.count(1))
+
+# print(list_a.index(2))
+
+# # copy(リストの中身のコピーを返す)
+# print(list_a)
+# list_b = list_a.copy()
+# list_b[0] = 'AAAAA'
+# print(list_a)
+
+# # sort(要素の昇順並び替え), reverse(要素の逆順並び替え)
+# list_a = ['banana', 'apple', 'lemon', 'grape']
+
+# print(list_a)
+# # list_a.sort()
+# list_a = sorted(list_a)
+# list_a.reverse()
+# print(list_a)
+# -----------------------------------------------------------------------
+
+# Dictionary(辞書型)
+
+# car = {'brand': 'Toyota', 'model': 'Prius', 'year': 2015, 1: 100}
+
+# print(car['brand'])
+# print(car.get('bran', 12))
+# print(car.get(1))
+
+# print(car.keys()) # キー一覧
+# print(car.values()) # 値一覧
+# print(car.items()) # キー　＋　値
+
+# for k, v in car.items():
+#   print('key = {}, value = {}'.format(k, v))
+
+# if 'brand' in car:
+#   print('carのブランドは{}'.format(car['brand']))
+
+# 辞書型のメソッド
+
+# car = {'brand': 'Toyota', 'model': 'Prius', 'year': 2015}
+
+# tmp_car = {'country': 'Japan', 'prefecture': 'Aichi', 'model': 'カローラ'}
+
+# car.update(tmp_car)  # 追加、更新
+# print(car)
+
+# car['city'] = 'Toyota-shi'
+# car['year'] = 2017
+# print(car)
+
+# value = car.popitem()  # 一番最後のアイテムを取り出す
+# print(car)
+# print(value)
+
+# value = car.pop('model')  # 値を取り出す
+# print(car)
+# print(value)
+
+# car.clear()  # 中身を削除
+# print(car)
+
+# del car  # del 変数そのもの削除
+# print(car)
+# ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+# タプルは、リストと似ていて値を複数入れる。変更追加できない,ハッシュ化して辞書型のキーとして利用する
+
+# fruit = ('apple', 'banana', 'lemon')
+
+# print(fruit)
+# print(type(fruit))
+# print(fruit[0])
+# # fruit[1] = 'grape' エラー
+# fruit = fruit + ('grape',)
+# print(fruit)
+
+# list_fruit = ['apple', 'banana']
+# fruit = tuple(list_fruit)  # タプル変換
+# print(fruit)
+# print(fruit.count('apple'))  # 要素の個数
+# print(fruit.index('apple'))  # 要素の場所、インデックス番号で,ない時はエラー
+
+# pos = (135, 35)
+
+# countries = {pos: 'Japan'}
+# print(countries.get((135, 35)))
+# ----------------------------------------------------------------------
+
+# セット
+
+# set_a = {'a', 'b', 'c', 'd', 'a', 12}
+# print(set_a)
+# print('e' in set_a) # 指定要素あるか？true false
+# print('a' in set_a)
+# print('a' not in set_a) # 指定要素がないか？ true false
+
+# print(len(set_a)) # 要素の個数
+
+# # add, remove, discard, pop, clear
+
+# set_a.add('A') # 追加
+# print(set_a)
+
+# set_a.remove('a') # 削除 存在しない要素を指定するとエラーになる
+# print(set_a)
+
+# set_a.discard(13) # 存在しない要素を指定してもエラーにならない
+# print(set_a)
+
+# val = set_a.pop() # 取り出し
+# print(val, set_a)
+
+# set_a.clear()
+# print(set_a) # 中身を空に
+
+# s = {'a', 'b', 'c', 'd'}
+# t = {'c', 'd', 'e', 'f'}
+
+# u = s | t  # 和集合
+# # u = s.union(t)
+# print(u)
+
+# u = s & t  # 積集合
+# # s.intersection(t)
+# print(u)
+
+# u = s - t  # 差集合 sに含まれていてtに含まれていない
+# # s.difference(t)
+# print(u)
+
+# u = s ^ t  # 対象差 両方に被っていない
+# # s.symmetric_difference(t)
+# print(u)
+
+# s |= t  # 要素を追加する
+# print(s)
+
+# # issubset, issuperset, isdisjoint
+# s = {'apple', 'banana'}
+# t = {'apple', 'banana', 'lemon'}
+# u = {'cherry'}
+
+# print(s.issubset(t))  # sはt「に」全部含まれてるか? true false
+# print(s.issuperset(t))  # sはt「を」全部含んでいるか? true false
+
+# print(t.isdisjoint(s)) # 重なっている要素がひとつもない場合true
+# print(t.isdisjoint(u))
+
