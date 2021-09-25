a0d0b72210ae3e38f6a957415f37ebd0cd1b3c39
sakamototaisei <taisei0403089@gmail.com>
2021-09-25 16:58:44 +0900

    study python import

---
+++ import.py
@@ -0,0 +1,21 @@
+# ライブラリの利用
+# import datetime
+# print(datetime.date.today())
+
+# from datetime import date
+# print(date.today())
+
+# import numpy as np
+# a = np.array([[1,2],[3,4]])
+# print(a)
+
+# import datetime
+# dir(datetime)
+
+# import requests
+# url = "https://sakataiblog.com"
+# response = requests.get(url)
+# print(response.status_code)
+
+from math import pi
+print('{0:.0f} {0:.2f} {0:.4f}'.format(pi))
--- practice.py
+++ practice.py
@@ -1,12 +1,27 @@
-fp = open('sample.txt', 'w')
-fp.write('This is sample1.\n')
-fp.write('This is sample2.\n')
-fp.close()
+# fp = open('sample.txt', 'w')
+# fp.write('This is sample1.\n')
+# fp.write('This is sample2.\n')
+# fp.close()
 
-# fp = open('sample.txt', 'r', encoding='utf-8')
-# fp.read()
-with open('sample.txt', 'r') as fp:
-  data = fp.read()
+# # fp = open('sample.txt', 'r', encoding='utf-8')
+# # fp.read()
+# with open('sample.txt', 'r') as fp:
+#   data = fp.read()
 
-for line in data:
-  print(line, end='')
+# for line in data:
+#   print(line, end='')
+
+try:
+  fp = open('sample.txt', 'r')
+  line = fp.readline()
+  num = int(line)
+except OSError:
+  print('OSのエラーです')
+except (ValueError, ZeroDivisionError, TypeError):
+  print('数値演算のエラー')
+except Exception:
+  print('一般的なエラーです')
+else:
+  print('結果は{}です。'.format(num))
+finally:
+  fp.close()
