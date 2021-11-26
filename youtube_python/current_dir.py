d6041a45770e03353fd37a7bab6527e4d9202bb9
sakamototaisei <taisei0403089@gmail.com>
2021-11-26 23:04:49 +0900

    python 100

--- youtube_python/current_dir.py
+++ youtube_python/current_dir.py
@@ -2,7 +2,8 @@ import os
 
 for curDir, dirs, files in os.walk('.'):
     for file in files:
-        print(f'{curDir}/{file}')
+        # ディレクトリとファイル名の結合でパスの作成が可能に
+        print(os.path.join(curDir, file))
 
 
 print('------------------------')
@@ -17,3 +18,23 @@ print('------------------------')
 
 # 絶対パスの取得 abspath()
 print(os.path.abspath('./Udemy_Python/udemy_4.py'))
+
+# ファイル名の取得 basename()
+print(os.path.basename('./Udemy_Python/udemy_4.py'))
+
+# ファイル・ディレクトリの存在確認
+print(os.path.exists('./Udemy_Python/udemy_4.py'))
+print(os.path.exists('PYTHON'))
+print(os.path.exists('FE_Python'))
+
+print('------------------------')
+
+# ディレクトリの存在確認
+print(os.path.isdir('PYTHON'))
+print(os.path.isdir('FE_Python'))
+
+print('------------------------')
+
+# ファイルに存在確認
+print(os.path.isfile('./Udemy_Python/udemy_4.py'))
+print(os.path.isfile('./Udemy_Python/udemy_48.py'))
