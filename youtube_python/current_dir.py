1b26e22b1a23bc9951e006d6c9e815836b139b49
sakamototaisei <taisei0403089@gmail.com>
2021-11-26 11:58:57 +0900

    絶対パスの取得

--- youtube_python/current_dir.py
+++ youtube_python/current_dir.py
@@ -3,3 +3,17 @@ import os
 for curDir, dirs, files in os.walk('.'):
     for file in files:
         print(f'{curDir}/{file}')
+
+
+print('------------------------')
+
+
+lists = os.listdir('.')
+lists.sort()
+print(lists)
+
+
+print('------------------------')
+
+# 絶対パスの取得 abspath()
+print(os.path.abspath('./Udemy_Python/udemy_4.py'))
