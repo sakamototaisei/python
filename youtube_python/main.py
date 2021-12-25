83448938c6f99b5c7c1adde75e74ef59bc153159
sakamototaisei <taisei0403089@gmail.com>
2021-12-25 22:54:43 +0900

    難易度を選択できる機能追加

--- youtube_python/main.py
+++ youtube_python/main.py
@@ -4,7 +4,7 @@ from termcolor import colored
 from inputimeout import inputimeout, TimeoutOccurred
 
 
-head_fruits = {
+fruits_head = {
     'グレープフルーツ': 'grapefruit',
     'パイナップル': 'pineapple',
     'イチゴ': 'strawberry',
@@ -19,7 +19,7 @@ head_fruits = {
     'ラズベリー': 'raspberry'
 }
 
-easy_fruits = {
+fruits_easy = {
     'リンゴ': 'apple',
     'バナナ': 'banana',
     'モモ': 'peach',
@@ -34,34 +34,28 @@ easy_fruits = {
     'パパイヤ': 'papaya'
 }
 
+def mode_choice():
+    while True:
+        mode = input('easyモードか、headモードを選択してください(整数入力). 1:easy, 2:head >>')
+        if mode not in('1', '2'):
+            print('正しく入力してください')
+            continue
+        # 使うdictを選択する
+        if mode == '1':
+            fruits = fruits_easy
+        elif mode == '2':
+            fruits = fruits_head
+        return fruits
 
-
+fruits = mode_choice()
 template = '*'*15 + '\n英単語: {}\n日本語(仮名文字)を入力してください: \n' + '*'*15
-
 mistake_cnt = 0
 correct_cnt = 0
 
 while True:
-    mode = input('easyモードか、headモードを選択してください(整数入力). 1:easy, 2:head >>')
-    if mode not in('1', '2'):
-        print('正しく入力してください')
-        continue
     # ランダムに辞書から英単語を表示する
-    if mode == '1':
-        word = random.choice(
-            list(easy_fruits.keys())
-        )
-        print(colored(
-            template.format(word), 'green')
-        )
-    elif mode == '2':
-        word = random.choice(
-            list(head_fruits.keys())
-        )
-        print(colored(
-            template.format(word), 'green')
-        )
-
+    word = random.choice(list(fruits.keys()))
+    print(colored(template.format(word), 'green'))
     try:
         # 自分が英単語を入力する
         answer = inputimeout(prompt='>>', timeout=7)
@@ -69,7 +63,7 @@ while True:
         if answer == '0':
             print('終了します')
             break
-        elif answer == easy_fruits[word] or answer == head_fruits[word]:
+        elif answer == fruits[word]:
             print(colored('正解', 'red'))
             correct_cnt += 1
             if correct_cnt == 5:
