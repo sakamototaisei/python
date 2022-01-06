2e8d77449fe7b3690dea706aaa416f65a2faaaa0
sakamototaisei <taisei0403089@gmail.com>
2022-01-06 14:12:17 +0900

    単方向リンクリストを逆方向のに並び替える

--- Udemy_algorithm/data/linkedlist.py
+++ Udemy_algorithm/data/linkedlist.py
@@ -55,18 +55,47 @@ class LinkedList(object):
         previous_node.next = current_node.next
         current_node = None
 
+    def reverse_iterative(self) -> None:
+        previous_node = None
+        current_node = self.head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = previous_node
+
+            previous_node = current_node
+            current_node = next_node
+
+        self.head = previous_node
+
+    # 上記のreverseをリカーシブルで実行
+    def reverse_recursive(self) -> None:
+        def _reverse_recursive(current_node: Node, previous_node: Node) -> Node:
+            if not current_node:
+                return previous_node
+
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+            return _reverse_recursive(current_node, previous_node)
+
+        self.head = _reverse_recursive(self.head, None)
+
 
 if __name__ == '__main__':
     l = LinkedList()
     l.append(1)
     l.append(2)
     l.append(3)
-    l.insert(0)
     # print(l.head.data)
     # print(l.head.next.data)
     # print(l.head.next.next.data)
     # print(l.head.next.next.next.data)
     l.print()
-    print('-----------')
-    l.remove(2)
+    print('----------- Iter')
+    l.reverse_iterative()
+    l.print()
+
+    print('----------- Rec')
+    l.reverse_recursive()
     l.print()
