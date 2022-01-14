78b08a88b8de89b0230f1b5e2aa5dcb3b0e4e4aa
sakamototaisei <taisei0403089@gmail.com>
2022-01-14 16:51:04 +0900

    回帰分析

--- Python_text/pybotweb/pybot_temp.py
+++ Python_text/pybotweb/pybot_temp.py
@@ -1,9 +1,14 @@
 import pandas
+import pickle
+from sklearn.metrics import r2_score
 
 
 def temp_command(command):
     # コマンドを分割して地名を取り出す
     cmd, station = command.split()
+    # 学習済みモデルのロード
+    with open('/Users/sakamototaisei/Desktop/python/Python_text/trained-reg-model.pickle', 'rb') as b:
+        reg = pickle.load(b)
 
     df = pandas.read_csv(
         '/Users/sakamototaisei/Desktop/python/Python_text/pybotweb/data.csv',
@@ -31,7 +36,21 @@ def temp_command(command):
         rounded_mean = round(mean, 1)
         response = '平均気温は{}度でした'.format(rounded_mean)
     else:
-        response = 'データが見つかりません'
+        try:
+            # 緯度はstation変数に入っている
+            latitude = float(station)
+            # 気温を予測する
+            predicted = reg.predict([[latitude]])
+            # 予測された気温を取り出す
+            predicted_temp = predicted[0]
+            rounded_temp = round(predicted_temp, 1)
+            # pybotの返事を作る
+            response = 'おそらく{}度くらい'.format(rounded_temp)
+        # 緯度以外が指定された場合
+        except ValueError:
+            response = '緯度を入力してください'
+
+        # response = 'データが見つかりません'
 
     return response
 
