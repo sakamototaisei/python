53c4715047f32fb6f56baa294a213b37a924d8e6
sakamototaisei <taisei0403089@gmail.com>
2022-05-21 20:18:43 +0900

    初回カード配って整理するカード配って整理するまで

--- Udemy_BaBa_Game/main.py
+++ Udemy_BaBa_Game/main.py
@@ -1,15 +1,7 @@
-"""
-list
-1~13
-egara(str)
-4 suit each card
-add baba
-53 card
-"""
+import random
+
+
 # ババ抜きの山札生成関数
-from venv import create
-
-
 def create_initial_deck() -> list:
     """
     Create a list object containing 53 cards.
@@ -35,6 +27,7 @@ def create_initial_deck() -> list:
 # sample = create_initial_deck()
 # print(sample)
 
+
 # プレイヤーの作成
 def create_player(name: str, is_auto: bool = True) -> dict:
     """
@@ -50,6 +43,60 @@ def create_player(name: str, is_auto: bool = True) -> dict:
 player1 = create_player('sakatai', is_auto=False)
 player2 = create_player('Apple')
 player3 = create_player('Banana')
-print(player1)
-print(player2)
-print(player3)
+# print(player1)
+# print(player2)
+# print(player3)
+
+
+# カードの分配機能
+initial_deck = create_initial_deck()
+
+player1 = create_player('A')
+player2 = create_player('B')
+# player3 = create_player('C')
+# player4 = create_player('D')
+# player5 = create_player('E')
+# players = [player1, player2, player3, player4, player5]
+# print(players)
+
+
+def initial_deal(initial_deck: list, *args: tuple) -> list:
+    """
+    Deal cards to players automatically
+    """
+    players = list(args)
+    # 山札をシャッフルさせる
+    random.shuffle(initial_deck)
+    # divmod(int, int) 商と余りを求めてくれる組み込み関数
+    q, mod = divmod(len(initial_deck), len(players))
+    # print(q, mod)
+    for i in range(len(players)):
+        slice_n = q
+        if i < mod:
+            slice_n = q + 1
+        players[i]["deck"] = initial_deck[:slice_n]
+        del initial_deck[:slice_n]
+    return players
+
+players = initial_deal(initial_deck, player1, player2)
+
+# print(len(players[0]["deck"]))
+# print(len(players[4]["deck"]))
+
+# カード整理(初回用)
+def initial_putdown(deck: list) -> list:
+    """
+    Play(put down) matching cards.
+    """
+    while len(set(deck)) != len(deck):
+        popped_card = deck.pop(0)
+        # ペアがあった場合
+        if popped_card in deck:
+            deck.remove(popped_card)
+        else:
+            deck.append(popped_card)
+    return deck
+
+deck = players[0]["deck"]
+print(deck)
+print(initial_putdown(deck))
